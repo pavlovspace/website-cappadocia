97bb2e6d78b3e34c3d8ec9eb738605f9f472a87b
Paweł Pavlov <pavlov1space@gmail.com>
2020-07-17 15:20:57 +0200

    fixed popup overlay

--- app/js/main.js
+++ app/js/main.js
@@ -1,104 +1,105 @@
 // -- preloader
 window.onload = function () {
-    this.document.querySelector('.preloader').style.display = 'none'
-}
+  this.document.querySelector(".preloader").style.display = "none";
+};
 
 // -- scroll paper animation
 
 const flightPath = {
-    curviness: 1.25,
-    autoRotate: true,
-    values: [{
-            x: 100,
-            y: -10
-        },
-        {
-            x: 300,
-            y: -50
-        },
-        {
-            x: 500,
-            y: 3
-        },
-        {
-            x: 950,
-            y: -50
-        },
-        {
-            x: 350,
-            y: -30
-        },
-        {
-            x: 600,
-            y: 5
-        },
-        {
-            x: 900,
-            y: 100
-        },
-        {
-            x: window.innerWidth,
-            y: -250
-        },
-    ]
-}
+  curviness: 1.25,
+  autoRotate: true,
+  values: [
+    {
+      x: 100,
+      y: -10,
+    },
+    {
+      x: 300,
+      y: -50,
+    },
+    {
+      x: 500,
+      y: 3,
+    },
+    {
+      x: 950,
+      y: -50,
+    },
+    {
+      x: 350,
+      y: -30,
+    },
+    {
+      x: 600,
+      y: 5,
+    },
+    {
+      x: 900,
+      y: 100,
+    },
+    {
+      x: window.innerWidth,
+      y: -250,
+    },
+  ],
+};
 
 const tween = new TimelineLite();
 
 tween.add(
-    TweenLite.to('.paper-plane', 1, {
-        bezier: flightPath,
-        ease: Power1.easeInOut
-    })
+  TweenLite.to(".paper-plane", 1, {
+    bezier: flightPath,
+    ease: Power1.easeInOut,
+  })
 );
 
 const controller = new ScrollMagic.Controller();
 
 const scene = new ScrollMagic.Scene({
-        triggerElement: '.scroll-animation',
-        duration: 1000,
-        triggerHook: 1,
-    })
-    .setTween(tween)
-    .addTo(controller)
+  triggerElement: ".scroll-animation",
+  duration: 1000,
+  triggerHook: 1,
+})
+  .setTween(tween)
+  .addTo(controller);
 
 // -- visual effect
 
 function scrollAppear() {
-    let targetEl = document.querySelectorAll('.transition');
-    targetEl.forEach(el => {
-        let targetElTop = el.getBoundingClientRect().top;
-        let windowHeight = window.innerHeight;
-        if (targetElTop <= windowHeight) {
-            el.classList.add('transition-appear');
-        } else if (targetElTop >= windowHeight) {
-            el.classList.remove('transition-appear');
-        }
-    })
+  let targetEl = document.querySelectorAll(".transition");
+  targetEl.forEach((el) => {
+    let targetElTop = el.getBoundingClientRect().top;
+    let windowHeight = window.innerHeight;
+    if (targetElTop <= windowHeight) {
+      el.classList.add("transition-appear");
+    } else if (targetElTop >= windowHeight) {
+      el.classList.remove("transition-appear");
+    }
+  });
 }
 
-window.addEventListener('scroll', scrollAppear)
+window.addEventListener("scroll", scrollAppear);
 
 // -- popup
-let popupBtn = document.getElementById('popupBtn')
-popupBtn.addEventListener('click', () => {
-    popupToggle()
-})
+let popupBtn = document.getElementById("popupBtn");
+popupBtn.addEventListener("click", () => {
+  popupToggle();
+});
 
 function popupToggle() {
-    let blur = document.getElementById('blur');
-    let popup = document.getElementById('popup');
-    document.querySelector('body').style.overflow = 'hidden';
+  let blur = document.getElementById("blur");
+  let popup = document.getElementById("popup");
+  document.querySelector("body").style.overflow = "hidden";
 
-    blur.classList.add('active')
-    popup.classList.add('active')
+  blur.classList.add("active");
+  popup.classList.add("active");
 }
 
-let closePopup = document.querySelector('.close')
-closePopup.addEventListener('click', () => {
-    let blur = document.getElementById('blur');
-    document.querySelector('body').style.overflow = 'auto';
+let closePopup = document.querySelector(".close");
+closePopup.addEventListener("click", () => {
+  let blur = document.getElementById("blur");
+  document.querySelector("body").style.overflow = "auto";
 
-    popup.classList.remove('active');
-    blur.classList.toggle('active')
+  popup.classList.remove("active");
+  blur.classList.toggle("active");
 });
