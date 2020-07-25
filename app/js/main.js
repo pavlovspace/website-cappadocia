22807e53505e9ee4e997813f6c04f799e1f06008
Paweł Pavlov <pavlov1space@gmail.com>
2020-07-25 14:17:40 +0200

    implemented Smooth Scrolling

--- app/js/main.js
+++ app/js/main.js
@@ -243,3 +243,41 @@ function sliderTrip() {
 }
 
 //-- Smooth Scrolling
+function smoothScroll(target, duration) {
+  var target = document.querySelector(target);
+  var targetPosition = target.getBoundingClientRect().top;
+  var startPosition = window.pageYOffset || window.scrollY;
+  var distance = targetPosition - startPosition;
+  var startTime = null;
+
+  function loop(currentTime) {
+    if (startTime === null) startTime = currentTime;
+    var timeElapsed = currentTime - startTime;
+    var run = ease(timeElapsed, startPosition, distance, duration);
+    window.scrollTo(0, run);
+    if (timeElapsed < duration) requestAnimationFrame(loop);
+  }
+  function ease(t, b, c, d) {
+    t /= d / 2;
+    if (t < 1) return (c / 2) * t * t + b;
+    t--;
+    return (-c / 2) * (t * (t - 2) - 1) + b;
+  }
+  requestAnimationFrame(loop);
+}
+
+//Animating 1 Link
+document.querySelector(".ourTripLink").addEventListener("click", function () {
+  smoothScroll("#ourTrip", 500);
+});
+
+//Animating 2 Link
+document.querySelector(".hotelLink").addEventListener("click", function (e) {
+  e.preventDefault();
+  smoothScroll("#hotel", 500);
+});
+// Animating 3 Link
+document.querySelector(".priceLink").addEventListener("click", function (e) {
+  e.preventDefault();
+  smoothScroll("#price", 500);
+});
